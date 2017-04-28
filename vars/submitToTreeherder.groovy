385eb07caf68cb813f1ff15f96ac4a415c062f8c
Jeremiah Orem <oremj@oremj.com>
2017-04-28 09:43:54 -0700

    submitToTreeherder: add comment with link to schema issue

--- vars/submitToTreeherder.groovy
+++ vars/submitToTreeherder.groovy
@@ -42,6 +42,11 @@ def call(String project,
   // TODO include ec2-metadata output in payload
   exchange = "exchange/${PULSE_USR}/jobs"
   routingKey = "${PULSE_USR}.${payload.productName}"
+
+  // Skipping schema validation due to https://github.com/mozilla/fxtest-jenkins-pipeline/issues/22
+  // schema = libraryResource 'org/mozilla/fxtest/pulse/schemas/treeherder.yml'
+  // publishToPulse(exchange, routingKey, JsonOutput.toJson(payload), schema)
+
   publishToPulse(exchange, routingKey, JsonOutput.toJson(payload))
   treeherderURL = "https://treeherder.mozilla.org/#/jobs?repo=${payload.productName}&revision=${payload.origin.revision}"
   echo "Results will be available to view at $treeherderURL"
